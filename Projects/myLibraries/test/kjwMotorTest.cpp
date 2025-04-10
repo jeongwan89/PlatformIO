3c7af24a1a017dcd57006b56622c97dbfa97b2ef
jeongwan89 <jeongwan89@mac.com>
2025-04-10 19:52:03 +0900

    핀 번호 조정 / delay 함수 절제

--- Projects/myLibraries/test/kjwMotorTest.cpp
+++ Projects/myLibraries/test/kjwMotorTest.cpp
@@ -6,7 +6,7 @@
 
 const int speedPin = 9; // PWM 핀
 const int directionPin = 8; // 디지털 핀
-const int analogPin = A2; // 아날로그 핀 A1
+const int analogPin = A1; // 아날로그 핀 A1
 const int sensorSensitivity = 185; // 5A 센서의 감도 (mV/A)
 const int analogSpeedPin = A0; // 추가된 아날로그 핀 A0
 const int CLK = 4; // TM1637 CLK 핀
@@ -56,7 +56,6 @@ void loop()
     // 방향 전환 테스트
     motor.ToggleDirection();
     Serial.println("Direction Toggled");
-    delay(2000);
 
     // 속도 증가 테스트
     for (int speed = 0; speed <= 100; speed += 10)
@@ -79,17 +78,14 @@ void loop()
     // 방향 전환 테스트
     motor.ToggleDirection();
     Serial.println("Direction Toggled");
-    delay(2000);
 
     // ACS712Sensor 테스트 코드
     float current = currentSensor.readCurrent(); // 전류 값 읽기
     Serial.print("Current: ");
     Serial.print(current);
     Serial.println(" A");
-    delay(1000); // 1초마다 값 읽기
 
     // TM1637 디스플레이에 전류 값 표시
     int displayValue = static_cast<int>(current * 100); // 전류 값을 소수점 두 자리까지 정수로 변환
     display.showNumberDec(displayValue, true, 4, 0); // 4자리로 표시
-    delay(1000); // 1초마다 업데이트
 }
